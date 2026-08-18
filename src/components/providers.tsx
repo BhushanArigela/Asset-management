54a37799767bb4dd06b4a3a512276e0bac5ae6ee
BhushanArigela <32087680+BhushanArigela@users.noreply.github.com>
2026-08-18 22:01:32 +0530

    Add files via upload

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <SessionProvider basePath={`${process.env.NEXT_PUBLIC_BASE_PATH || ""}/api/auth`}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
