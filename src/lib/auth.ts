50f99cf9a72f223ea20f8c4975c027db52685a8d
BhushanArigela <32087680+BhushanArigela@users.noreply.github.com>
2026-08-19 09:10:10 +0530

    Add files via upload

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,8 +11,8 @@ const loginSchema = z.object({
 });
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  debug: true,
   trustHost: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/api/auth` : "/api/auth",
   ...authConfig,
   providers: [
     Credentials({
